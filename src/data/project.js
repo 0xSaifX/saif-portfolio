364b7ac305c5e4774bc386ee63087946d47a8118
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-08 09:53:12 +0100

    auto-commit

--- src/data/project.js
+++ src/data/project.js
@@ -1,21 +1,21 @@
 export const projects = [
   {
     title: "Real NPL Commerce Page",
-    description: "Responsive real NPL landing page with modern UI.",
+    description: "Responsive real NPL landing page with modern UI, reusable UI components and deployed with Git and Github.",
     tech: ["React", "Tailwind", "Framer Motion"],
     github: "https://github.com/0xSaifX/My-Npl-Project",
     live: "https://saif-npl-project.vercel.app",
   },
   {
     title: "Edusity Platform UI",
-    description: "Fully Responsive School website with reusable components.",
+    description: "Fully Responsive School website with reusable and responsive components pixel-accurate designs for all screen sizes.",
     tech: ["React", "Tailwind"],
     github: "https://github.com/0xSaifX/My-learning-roadmap1",
     live: "https://saif-scooter.vercel.app",
   },
   {
-    title: "Flutter Interface App",
-    description: "Flutter with Admin dashboard UI with responsive layout for mobile app.",
+    title: "Dashboard Interface (eCommerce UI)",
+    description: "Designed and implemented a modern eCommerce landing page with real-world UI pattern.",
     tech: ["React", "Tailwind"],
     github: "https://github.com/0xSaifX/Scooter-eCommerce-App-created",
     live: "https://saifx.netlify.app",
