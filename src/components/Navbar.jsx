f9c3f30f38ccbf152b09c1115180470bff176abb
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-20 23:01:08 +0100

    auto-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
           Saif
         </h1>
 
-        <div className="flex items-center gap-6 text-sm text-white">
+        <div className="flex items-center gap-6 text-sm text-zinc-200">
           <a href="#projects" className="hover:text-emerald-400 no-underline transition">
             Projects
           </a>
